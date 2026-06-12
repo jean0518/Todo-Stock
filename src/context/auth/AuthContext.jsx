e0abf7a736ee41cc08c9e06a1f531dccf7884dca
jean0518 <sotoortizjeanmichael@gmail.com>
2026-06-11 22:55:32 -0500

    Update context

--- src/context/auth/AuthContext.jsx
+++ src/context/auth/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import {supabase} from "../index"
+import {supabase} from "../../index"
 const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
